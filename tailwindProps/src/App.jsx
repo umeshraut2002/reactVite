ea46881b07cd2d5f2eccd3d146a7d8fdb2c62293
umesh raut <uraut6044@gmail.com>
2024-09-18 20:25:28 +0530

    App.jsx add card method card in card.jsx use props

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card  from './components/card'
+
+function App() {
+
+  return (
+    <>
+  <h1 className="bg-blue-700 text-white p-4 rounded-xl">Hello Tailwind</h1>
+  <Card username = "Umesh" textBtn = "Click Me"/>
+</>
+
+  )
+}
+
+export default App
