ff4fd8f96c1e207de3d8599eb39e19b5471c35bf
umesh raut <uraut6044@gmail.com>
2024-10-04 17:08:21 +0530

    maincontent

--- componentprac/src/components/Footer.jsx
+++ componentprac/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='w-full h-full p-0 m-0'>
-            <footer className='w-full h-12 bg-orange-700'>
-                <p>All right reserve @copy; 2024-25</p>
+            <footer className='w-full h-12 bg-orange-700 rounded-xl '>
+                <p className='font-bold uppercase'>All right reserve &copy; 2024-25</p>
             </footer>
         </div>
     )
--- componentprac/src/components/Maincontent.jsx
+++ componentprac/src/components/Maincontent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Maincontent = () =>{
     return(
-        <div className='p-5 m-5'>
+        <div className='items-center p-5 m-5'>
             <img className='border-2 rounded-xl' src='https://images.unsplash.com/photo-1726910133626-9b573eca70ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8'/>
         </div>
     )
