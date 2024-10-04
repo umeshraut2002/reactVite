fd2b6bbc95ca4e7d42fd6a7451fb08969a9dd375
umesh raut <uraut6044@gmail.com>
2024-10-04 17:27:32 +0530

    massage components

--- componentprac/src/App.jsx
+++ componentprac/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Maincontent from './components/Maincontent'
+import Massage from './components/Massage'
 
 function App() {
 
   return (
     <>
     <Navbar />
+    <Massage />
     <Maincontent />
     <Footer />
     </>
---
+++ componentprac/src/components/Massage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Massage = () => {
+    return (
+        <di>
+            <h1>Hello World</h1>
+            <p>Welcome to learning JSX! </p>
+        </di>
+    )
+}
+
+export default Massage;
