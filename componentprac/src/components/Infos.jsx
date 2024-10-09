39f33e5dd8a9acf8f6310054d40da306c1408da1
umesh raut <uraut6044@gmail.com>
2024-10-09 15:54:35 +0530

    Infos Componenet added

--- componentprac/src/components/Infos.jsx
+++ componentprac/src/components/Infos.jsx
@@ -4,18 +4,19 @@ const Infos = () => {
     return (
         <>
          <User2 
-            img=""
-            name="Umesh",
+            img="https://avatars.githubusercontent.com/u/85052811?v=4"
+            name="Jonas"
             age={29}
-            location="Nagpur"
+            location="USA"
          />
         </>
     )
 }
 
-const User2 = ({name, age, location}) => {
+const User2 = ({img,name, age, location}) => {
     return (
         <>
+        <img src={img} className="items-center bg-cover w-50 h-50 rounded-xl"></img>
         <h2>Name : {name}</h2>
         <h2>Age : {age}</h2>
         <h2>Location : {location}</h2>
