03996f9417e608553bbb9bf39558e0076078e589
umesh raut <uraut6044@gmail.com>
2024-10-13 18:05:29 +0530

    friends component

--- stateprac/src/App.jsx
+++ stateprac/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './componenets/Counter'
+import Friends from './componenets/Friends'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Counter />
+      <Friends />
     </>
   )
 }
---
+++ stateprac/src/componenets/Friends.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const Friends = () => {
+    const [frnd, setFrnd] = useState(["John", "Scarlet", "stevian"]);
+
+    const handleClick = () =>{
+        setFrnd([...frnd, "Umesh", "Vihaan"]);
+    }
+
+    const removeFrnd = () => {
+        setFrnd(frnd.filter(f => f !== "Umesh"))
+    }
+
+    const updateFrnd = () =>{
+        setFrnd(frnd.map(f => f === "Umesh" ? "Umesh raut" : f))
+    }
+
+    return(
+        <>
+            {frnd.map(f => (
+                <li key={Math.random()}>{f}</li>
+            ))} 
+
+            <button onClick={handleClick}>Add New Friends</button>
+            <button onClick={removeFrnd}>Remove Friend</button>
+            <button onClick={updateFrnd}>Update</button>
+        </>
+    )
+}
+
+export default Friends;
