cbe0ddb62d4e87a384028068ac5113e8569f3ebf
umesh raut <uraut6044@gmail.com>
2024-10-14 19:44:10 +0530

    ui profile and state added

--- stateprac/src/componenets/Profile.jsx
+++ stateprac/src/componenets/Profile.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () =>{
+
+    const [profile, setProfile] = useState({
+        name: "Umesh",
+        age: 0
+    });
+
+    const [name, setName] = useState("");
+    const [age, setAge] =useState(0);
+
     return(
         <>
-            
+        <form onSubmit={handleSubmit}>
+        <input type="text" value={setName} onChange={handleOnChange} placeholder="Enter Name Here" ></input>
+        <button type="submit">Submit</button>
+        </form>
+
+        <form onSubmit={handleSubmit} >
+            <input type="number" value={setAge} onChange={handleChange} placeholder="Enter Age Here" ></input>
+        </form>
         </>
     )
 }
