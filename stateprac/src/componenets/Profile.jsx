014cc50c0d0cd9fba0cf9dfa290a82e5b32e9571
umesh raut <uraut6044@gmail.com>
2024-10-14 20:32:42 +0530

    profile information object

--- stateprac/src/componenets/Profile.jsx
+++ stateprac/src/componenets/Profile.jsx
@@ -3,26 +3,38 @@ import React, { useState } from "react";
 const Profile = () =>{
 
     const [profile, setProfile] = useState({
-        name: "Umesh",
+        name: '',
         age: 0
     });
 
     const [name, setName] = useState("");
     const [age, setAge] =useState(0);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setProfile([...profile, {name: name, age : age}])
+        setName('');
+        setAge(0);
+    }
+
     return(
         <>
+        <div>
         <h1>Profile Information</h1>
         <form onSubmit={handleSubmit}>
-        <input type="text" value={setName} onChange={handleOnChange} placeholder="Enter Name Here" ></input>
+        <input type="text" value={name} onChange={setName(e => e.target.value)} placeholder="Enter Name Here" ></input>
         <button type="submit">Submit</button>
         </form>
+        </div>
 
-        <form onSubmit={handleSubmit} >
-            <input type="number" value={setAge} onChange={handleChange} placeholder="Enter Age Here" ></input>
+        <div>
+        <form onSubmit={handleSubmit}>
+            <input type="number" value={age} onChange={setAge(e.target.value)} placeholder="Enter Age Here" ></input>
             <button type="submit">Submit</button>
         </form>
+        </div>
 
+        <div>
         <h3>Information</h3>
         {profile.map((item, index) => (
             <li key={index}>
@@ -30,6 +42,7 @@ const Profile = () =>{
                 Age: {item.age}
             </li>
         ))}
+        </div>
         </>
     )
 }
