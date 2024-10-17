3060e18f49f7108f6e48979d8f99e581585f2e75
umesh raut <uraut6044@gmail.com>
2024-10-17 16:49:14 +0530

    main  updated

--- routerProject/src/Root.jsx
+++ routerProject/src/Root.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Herosection from './components/Herosection'
+import Mainsection from './components/Mainsection'
 
 const Root = () => {
     return(
         <>
         <Navbar />
+        <Herosection />
+        <Mainsection />
         <Outlet />
         <Footer />
         </>
--- routerProject/src/main.jsx
+++ routerProject/src/main.jsx
@@ -3,13 +3,29 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-
 import About from './components/About.jsx'
+import Root from './Root.jsx'
+import Contact from './components/Contact.jsx'
+import Github from './components/Github.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/About",
-    element: <About />
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "github",
+        element: <Github />
+      }
+    ]
   }
 ]);
 
