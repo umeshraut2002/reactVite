eae0b6cd0d6931c0de8cf8903827896692a40d15
umesh raut <uraut6044@gmail.com>
2024-10-17 16:24:06 +0530

    Root added

--- routerProject/src/main.jsx
+++ routerProject/src/main.jsx
@@ -3,18 +3,18 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import Navbar from './components/Navbar.jsx'
 
-const router =createBrowserRouter([
+import About from './components/About.jsx'
+
+const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Navbar />
+    path: "/About",
+    element: <About />
   }
-])
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
     <RouterProvider router={router} />
   </StrictMode>,
 )
