95df6b02259109869b74060c58458188f30a1834
umesh raut <uraut6044@gmail.com>
2024-10-17 15:08:57 +0530

    browser router used to implement on componenet

--- routerProject/src/main.jsx
+++ routerProject/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import Navbar from './components/Navbar.jsx'
+
+const router =createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
