d2e2a7f00cd927b90cfb7cd1056b4015d235b447
umesh raut <uraut6044@gmail.com>
2024-10-23 20:46:39 +0530

    main updated

--- routerProject/src/main.jsx
+++ routerProject/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import App from './App.jsx'
 import './index.css'
 import About from './components/About.jsx'
 import Root from './Root.jsx'
