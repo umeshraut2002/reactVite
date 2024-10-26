55a176ea45dc3c91679d717d037faa1bade9871b
umesh raut <uraut6044@gmail.com>
2024-10-26 17:44:28 +0530

    layout main updated

--- MovieZone/src/component/layout/Main.jsx
+++ MovieZone/src/component/layout/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
                     </h3>
                     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
                         {/* Example Movie Card */}
-                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500">
+                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500 flex flex-col items-center">
                             <img
                                 src="https://posters.movieposterdb.com/24_04/2024/11152168/s_if-movie-poster_006c6837.jpg"
                                 className="mb-4 mr-4 bg-center bg-cover rounded-lg w-96 h-96"
@@ -24,7 +24,7 @@ const Main = () => {
                         </div>
 
                         {/* Repeat similar cards for other movies */}
-                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500">
+                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500 flex flex-col items-center">
                             <img
                                 src="https://posters.movieposterdb.com/24_06/2024/11315808/l_joker-folie-a-deux-movie-poster_8b8497ec.jpg"
                                 className="mb-4 mr-4 bg-center bg-cover rounded-lg w-96 h-96"
@@ -40,7 +40,7 @@ const Main = () => {
                         </div>
 
 
-                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500">
+                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500 flex flex-col items-center">
                             <img
                                 src="https://posters.movieposterdb.com/24_06/2024/6263850/l_deadpool-wolverine-movie-poster_36763292.jpg"
                                 className="mb-4 mr-4 bg-center bg-cover rounded-lg w-96 h-96"
@@ -55,10 +55,7 @@ const Main = () => {
                             </button>
                         </div>
 
-
-
-
-                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500">
+                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500 flex flex-col items-center">
                             <img
                                 src="https://posters.movieposterdb.com/22_02/2022/1877830/l_1877830_764432ad.jpg"
                                 className="mb-4 mr-4 bg-center bg-cover rounded-lg w-96 h-96"
@@ -73,12 +70,7 @@ const Main = () => {
                             </button>
                         </div>
 
-
-
-
-
-
-                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500">
+                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500 flex flex-col items-center">
                             <img
                                 src="https://posters.movieposterdb.com/21_08/2021/1160419/l_1160419_565d3d10.jpg"
                                 className="mb-4 mr-4 bg-center bg-cover rounded-lg w-96 h-96"
@@ -93,11 +85,7 @@ const Main = () => {
                             </button>
                         </div>
 
-
-
-
-
-                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500">
+                        <div className="p-4 transition-shadow duration-300 bg-gray-900 rounded-lg shadow-lg hover:shadow-orange-500 flex flex-col items-center">
                             <img
                                 src="https://posters.movieposterdb.com/24_06/2024/29268110/l_smile-deluxe-movie-poster_85506969.png"
                                 className="mb-4 mr-4 bg-center bg-cover rounded-lg w-96 h-96"
@@ -113,9 +101,11 @@ const Main = () => {
                         </div>
                         
                     </div>
+                    <div className='flex justify-center mt-10'>
                     <button className="px-4 py-2 mt-10 text-white bg-orange-600 rounded-lg hover:bg-orange-800">
                                 Show More 
                             </button>
+                            </div>
                 </main>
             </div>
         </>
