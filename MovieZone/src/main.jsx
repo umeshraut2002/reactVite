787333ee90e8f4d987fa14ed4e5f3139a7437e73
umesh raut <uraut6044@gmail.com>
2024-10-26 18:57:27 +0530

    main in diff method

--- MovieZone/src/main.jsx
+++ MovieZone/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import React from 'react';
 import './index.css';
 import { createRoot } from 'react-dom/client';
-import {RouterProvider,createBrowserRouter} from 'react-router-dom';
+import {Route, RouterProvider,createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 import Root from './Root.jsx';
 import About from './component/about/About.jsx';
 import Blog from './component/blog/Blog.jsx';
@@ -11,34 +11,47 @@ import Contact from './component/contact/Contact.jsx';
 import Home from './component/Home/Home.jsx'
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "home",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "blog",
-        element: <Blog />
-      },
-      {
-        path: "movies",
-        element: <Movies />
-      },
-      {
-        path: "contact",
-        element: <Contact />
-      }
-    ]
-  }
-])
+// this is method one to route the element 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       {
+//         path: "home",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "blog",
+//         element: <Blog />
+//       },
+//       {
+//         path: "movies",
+//         element: <Movies />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+// this is method 2nd to routr the element 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path= "/"  element= {<Root />}>
+      <Route path="home" element= {<Home />} />
+      <Route path="about" element= {<About />} />
+
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
