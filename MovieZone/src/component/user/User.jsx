5801cdf3b2e01f88b67446e1d0a405a5f4cba71a
umesh raut <uraut6044@gmail.com>
2024-10-27 10:14:34 +0530

    User component added

---
+++ MovieZone/src/component/user/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <>
+            <div className='bg-gray-500 min-h-56 rounded-lg'>
+                <h1>User: {}</h1>
+            </div>
+        </>
+    )
+}
+
+export default User;
