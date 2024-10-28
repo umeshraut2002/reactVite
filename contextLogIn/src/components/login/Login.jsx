1be288f4bf1769ead34bcf39eb7cd2cefbdc02eb
umesh raut <uraut6044@gmail.com>
2024-10-28 13:40:58 +0530

    tailwind add

--- contextLogIn/src/components/login/Login.jsx
+++ contextLogIn/src/components/login/Login.jsx
@@ -2,6 +2,20 @@ import React from "react";
 
 const Login = () => {
 
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <>
+            <div>
+                <h1>log in</h1>
+                <input type="text" placeholder="@username" ></input>
+                <input type="text" placeholder="Password" ></input>
+                <button onClick={handleSubmit} >Submit</button>
+            </div>
+        </>
+    )
 }
 
 export default Login;
