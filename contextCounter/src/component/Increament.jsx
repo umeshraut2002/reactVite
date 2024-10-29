fde856b22bdacf38b59e9bcd90de442b0ab7394c
umesh raut <uraut6044@gmail.com>
2024-10-29 11:32:24 +0530

    increament compo created

---
+++ contextCounter/src/component/Increament.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Increament = () => {
+    return(
+        <>
+            <h1>I Am Incrementar: </h1>
+            <button>Increment</button>
+        </>
+    )
+}
+
+export default Increament;
