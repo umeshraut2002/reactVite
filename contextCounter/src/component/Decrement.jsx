d20ba54a1a00cf5cb1ef0729cc2c9d2335efaba3
umesh raut <uraut6044@gmail.com>
2024-10-29 11:29:52 +0530

    componenets created

---
+++ contextCounter/src/component/Decrement.jsx
@@ -0,0 +1,12 @@
+import React form 'react';
+
+const Decrement = () => {
+    return(
+        <>
+            <h1>I am Decrementar : </h1>
+            <button>Decrement</button>
+        </>
+    )
+}
+
+export default Decrement;
