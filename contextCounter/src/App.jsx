4125df879a1b4bd945ee74fa02d26ef68daa02c3
umesh raut <uraut6044@gmail.com>
2024-10-29 11:41:57 +0530

    Context created context provider created props passed

--- contextCounter/src/App.jsx
+++ contextCounter/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CountContext } from './context/CountContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <CountContext.Provider value={count}>      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      </CountContext.Provider>
     </>
   )
 }
