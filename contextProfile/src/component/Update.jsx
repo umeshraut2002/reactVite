b36f9a2e716c9b5375a1d2d433643e303e36f3ab
umesh raut <uraut6044@gmail.com>
2024-11-02 14:19:58 +0530

    Componenets are changed

--- contextProfile/src/component/Update.jsx
+++ contextProfile/src/component/Update.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-const Update = () => {
+const UUpdate = () => {
     return(
         <>
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-4">
-        <div className="flex justify-between">
-          <button className="w-full bg-orange-500 text-white py-2 px-4 rounded-l-lg font-medium hover:bg-orange-600 transition-colors duration-300">
-            Update Profile
-          </button>
-        </div>
-        </div>
+        
         </>
     )
 }
 
-export default Update;
+export default UUpdate;
