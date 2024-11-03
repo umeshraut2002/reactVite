f90bee042829213f973c73a685bf2cd9e5a8cd31
umesh raut <uraut6044@gmail.com>
2024-11-03 12:17:23 +0530

    app jsx updated

--- contextTwoBtn/src/App.jsx
+++ contextTwoBtn/src/App.jsx
@@ -3,14 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Info from './component/Info'
+import UserContextProvider from './context/UserContextProvider'
+import Update from './component/Update'
+import Reset from './component/Reset'
+import Show from './component/Show'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Info />
-    </>
+    <UserContextProvider>
+      <Info />
+      <Update />
+      <Reset />
+      <Show />
+    </UserContextProvider>
   )
 }
 
