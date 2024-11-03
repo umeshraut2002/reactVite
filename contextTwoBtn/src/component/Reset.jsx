7d4312585fbf4081c3a36ddb02c789864f088ac4
umesh raut <uraut6044@gmail.com>
2024-11-03 12:48:34 +0530

    reset updated

--- contextTwoBtn/src/component/Reset.jsx
+++ contextTwoBtn/src/component/Reset.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 const Reset = () => {
-    return(
-     
+    const {user} = useContext(UserContext);
+
+    if(!user){
+        return <div><h2>Please Enter All Info</h2></div>
+    }
+    else{
+        return <div><h2>Name: {user.name} Surname: {user.surname} Age: {user.age}</h2></div>
+    }
 }
 
 export default Reset;
