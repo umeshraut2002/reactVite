0f2d15f2a51fb08e93782ace6e521e85492dfd1c
umesh raut <uraut6044@gmail.com>
2024-11-03 12:15:11 +0530

    context provider created

---
+++ contextTwoBtn/src/context/UserContextProvider.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { UserContext } from "./UserContext";
+import React from "react";
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    return(
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
