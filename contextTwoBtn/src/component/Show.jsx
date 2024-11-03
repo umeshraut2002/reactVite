91581ca06eb8a6c24cca21faf117de6a0087d368
umesh raut <uraut6044@gmail.com>
2024-11-03 12:58:40 +0530

    show compo updated

--- contextTwoBtn/src/component/Show.jsx
+++ contextTwoBtn/src/component/Show.jsx
@@ -1,11 +1,16 @@
 import React  from "react";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Show = () => {
-    return(
-        <>
+    const {user} = useContext(UserContext);
 
-        </>
-    )
+    if(!user){
+        return <div><h2>PLease Enter all Details </h2></div>
+    }
+    else{
+        return <div><h2>Name: {user.name} Username: {user.surname} Age: {user.age}</h2></div>
+    }
 }
 
 export default Show;
