72104ea80c544cb315a4d37df3844c56546fb22b
umesh raut <uraut6044@gmail.com>
2024-11-03 12:39:57 +0530

    info.jsx updated

--- contextTwoBtn/src/component/Info.jsx
+++ contextTwoBtn/src/component/Info.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import { useState } from 'react';
-import Show from './Show';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 const Info = () => {
 
     const [name, setName] = useState("");
     const [surname, setSurname] = useState("");
     const [age, setAge] = useState(0);
+
+    const {setUser} = useContext(UserContext);
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+
+        if(age){
+            setUser({age, setAge});
+        }
+    }
+
+    const handleReset = (e) => {
+        e.preventDefault();
+
+        if(name && surname && age){
+            setUser({name, setName, surname, setSurname, age, setAge});
+        }
+    }
+
+    const handleShow = (e) => {
+        e.preventDefault();
+
+        if(name && surname && age){
+            setUser({name, setName, surname, setSurname, age, setAge})
+        }
+    }
 
     return(
         <>
@@ -15,9 +42,11 @@ const Info = () => {
                 <h2>Name : {name}</h2>
                 <h2>Surname: {surname}</h2>
                 <h2>Age: {age}</h2>
+                {""}
                 <input 
                 type='text'
                 value={name}
+                onChange={(e) => setName(e.target.value)}
                 placeholder='Enter Your Name Here'
                 />
 
@@ -26,6 +55,7 @@ const Info = () => {
                 <input 
                 type='text'
                 value={surname}
+                onChange={(e) => setSurname(e.target.value)}
                 placeholder='Enter Your Surname Here'
                 />
 
@@ -35,13 +65,14 @@ const Info = () => {
                 type='number'
                 value={age}
                 placeholder='Enter Your Age Here'
+                onChange={(e) => setAge(e.target.value)}
                 />
 
                 <button onClick={handleUpdate}>Update</button>
                 {" "}
                 <button onClick={handleReset}>Reset</button>
                 {" "}
-                <button onClick={Show}>Show</button>
+                <button onClick={handleShow}>Show</button>
                 {" "}
             </div>
         </>
