3d40c843da35ca728dbeeeafbad7bbaf4f9cd81d
umesh raut <uraut6044@gmail.com>
2024-11-03 14:49:12 +0530

    user context updated

--- contextProfile/src/App.jsx
+++ contextProfile/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from i'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './component/Profile'
 import UserContextProvider from './context/UserContextProvider'
--- contextProfile/src/context/UserContext.js
+++ contextProfile/src/context/UserContext.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const UserConte = createContext();
+export const UserConte = createContext(null);
