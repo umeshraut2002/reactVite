169081ffc80d83a4cbcedb478f4f6c102de8a22e
umesh raut <uraut6044@gmail.com>
2024-11-03 12:22:50 +0530

    info compo

--- contextTwoBtn/src/component/Info.jsx
+++ contextTwoBtn/src/component/Info.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
+import Show from './Show';
 
 const Info = () => {
+
+    const [name. setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [age, setAge] = useState(0);
+
     return(
         <>
             <div>
                 <h2>Enter Your Information Here: </h2>
-                <h2>Name : {}</h2>
-                <h2>Username: {}</h2>
-                <h2>Age: {}</h2>
+                <h2>Name : {name}</h2>
+                <h2>Surname: {surname}</h2>
+                <h2>Age: {age}</h2>
                 <input 
                 type='text'
-                // value={}
+                value={name}
                 placeholder='Enter Your Name Here'
                 />
 
@@ -18,7 +25,7 @@ const Info = () => {
 
                 <input 
                 type='text'
-                // value={}
+                value={surname}
                 placeholder='Enter Your Surname Here'
                 />
 
@@ -26,15 +33,15 @@ const Info = () => {
 
                 <input 
                 type='number'
-                // value={}
+                value={age}
                 placeholder='Enter Your Age Here'
                 />
 
-                <button>Update</button>
+                <button onClick={handleUpdate}>Update</button>
                 {" "}
-                <button>Reset</button>
+                <button onClick={handleReset}>Reset</button>
                 {" "}
-                <button>Info</button>
+                <button onClick={Show}>Show</button>
                 {" "}
             </div>
         </>
