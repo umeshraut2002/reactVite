acc3566f848cbaa34580d4c073a6632b85a321f6
umesh raut <uraut6044@gmail.com>
2024-11-09 07:49:48 +0530

    App jsx updated

--- countercontext2/src/App.jsx
+++ countercontext2/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { contextProvider } from './context/countContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <contextProvider value={{count, setCount}}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      
+    </contextProvider>
   )
 }
 
