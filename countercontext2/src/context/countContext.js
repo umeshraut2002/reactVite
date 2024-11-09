2f207cda718fecd860013c8c567fe8e1112307d3
umesh raut <uraut6044@gmail.com>
2024-11-09 07:37:54 +0530

    return context added

--- countercontext2/src/context/countContext.js
+++ countercontext2/src/context/countContext.js
@@ -5,5 +5,5 @@ export const countContext = createContext();
 export const contextProvider = countContext.Provider
 
 export default function useCont(){
-    useContext(countContext);
+    return useContext(countContext);
 }
