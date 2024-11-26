cc7f613f99a19d155bc370a234d9b8a8ee33527b
umesh raut <uraut6044@gmail.com>
2024-11-26 14:41:50 +0530

    counter2 updated

--- countercontext3/src/components/Counters2.jsx
+++ countercontext3/src/components/Counters2.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Counters2 = () => {
     return(
         <div>
-         <h1>Counter 2: </h1>
+         <h1>I AM Counter 2 : Multiple with 4: {count} </h1>
         </div>
     )
 }
